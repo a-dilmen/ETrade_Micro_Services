9d11d1de25a9f8df7ef6271cfa2285510031b3e8
a-dilmen <abdulkadirdilmen2@gmail.com>
2023-03-05 16:49:11 +0300

    Auth micro services package and exception structure created

---
+++ AuthService/src/main/java/com/dilmen/constants/RestEndPoints.java
@@ -0,0 +1,28 @@
+package com.dilmen.constants;
+
+public class RestEndPoints {
+        public static final String VERSION = "/v1";
+        public static final String API = "/api";
+
+
+        public static final String DEV = "/dev";
+        public static final String TEST = "/test";
+
+
+        public static final String AUTH = "/auth";
+
+
+        public static final String SAVE = "/save";
+        public static final String UPDATE = "/updatee";
+        public static final String DELETE = "/delete";
+        public static final String FINDALL = "/findall";
+        public static final String FINDBYID = "/findbyid";
+        public static final String REGISTER = "/register";
+        public static final String LOGIN = "/login";
+
+
+
+
+
+
+}
---
+++ AuthService/src/main/java/com/dilmen/repository/entity/Auth.java
@@ -0,0 +1,16 @@
+package com.dilmen.repository.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Auth {
+
+        Long id;
+}
